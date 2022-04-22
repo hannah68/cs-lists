eafe82ccabac376b971154a042c987bde6432599
hannah68 <reihaneh.naderi1989@gmail.com>
2022-04-22 11:29:17 +0100

    implement stack

--- src/stack.mjs
+++ src/stack.mjs
@@ -1,32 +1,58 @@
 /* 
-Implementation of a Stack as a linked list
+Implementation of a Stack as a linked list(LIFO)
 */
-
 export const stack = () => {
 
     let size = 0;
     let head = null;
+    /* 
+    itereate through the list and then add the node to the last node in the list
+    */
+    function push(data) {
+        let newNode = {
+            data : data, 
+            next : head
+        };
+        head = newNode;
+        size++;
+    }
 
     /* 
-    Conceptual description
+    Iterate through the list and grab the last item and remove it from list
     */
-    function push(data) {}
-
-    /* 
-    Conceptual description
-    */
-    function pop() {}
+    function pop() {
+        const oldHead = head;
+        head = oldHead.next;
+        size--;
+        return oldHead.data;
+    }
 
     /* 
     Conceptual descriptions of each of the next 2 functions
     */
-    function top() {}
-    function getSize() {}
+    function top() {
+        return head.data;
+    }
+    function getSize() {
+        return size;
+    }
 
     /* 
     Conceptual description
     */
-    function toArray() {}
+    function toArray() {
+        const list = [];
+        let current = head;
+        while (current) {
+            list.push(current.data);
+            current = current.next;
+        }
+        
+        return list;     
+    }
 
     return { push, pop, top, getSize, toArray };
 }
+
+
+stack()
